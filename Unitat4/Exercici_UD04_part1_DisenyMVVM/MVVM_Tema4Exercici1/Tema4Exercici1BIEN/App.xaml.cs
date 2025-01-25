867e54dbfeb6391aed28275a8820cb69ae1aadf2
MalvaLego <nataliacoronado555@gmail.com>
2025-01-25 16:50:44 +0100

    add MVVM

---
+++ Unitat4/Exercici_UD04_part1_DisenyMVVM/MVVM_Tema4Exercici1/Tema4Exercici1BIEN/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Tema4Exercici1BIEN
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
